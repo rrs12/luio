b174ae965305fbac965cd12c723a4d8e0c2075ff
rrs12 <rahilsalecha12@gmail.com>
2023-11-27 18:49:07 +0530

    update

--- script.js
+++ script.js
@@ -27,3 +27,14 @@ var typed = new Typed(".typing", {
     backSpeed: 100,
     loop: true
 });
+
+let $root = $('html, body');
+$('a[href^="#"]').click(function() {
+    let href = $.attr(this, 'href');
+    $root.animate({
+      scrollTop: $(href).offset().top
+    }, 500, function() {
+      window.location.hash = href;
+    });
+    return false;
+  });
